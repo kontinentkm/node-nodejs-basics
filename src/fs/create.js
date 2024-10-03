5be580675f47ecb12885409539d59763092afdef
Max <KontinentKM@gmail.com>
2024-10-03 10:58:49 +0300

    feat: copy

--- src/fs/create.js
+++ src/fs/create.js
@@ -3,22 +3,17 @@ import path from "path";
 
 const create = async () => {
   try {
-    // Определяем путь к файлу fresh.txt в папке files
     const filePath = path.join(process.cwd(), "src/fs/files", "fresh.txt");
 
-    // Проверяем, существует ли файл
     try {
       await fs.access(filePath);
-      // Если файл существует, выбрасываем ошибку
       throw new Error("FS operation failed");
     } catch (error) {
-      // Если файл не найден (ошибка ENOENT), продолжаем создание файла
       if (error.code !== "ENOENT") {
         throw error;
       }
     }
 
-    // Создаём файл fresh.txt с содержимым "I am fresh and young"
     await fs.writeFile(filePath, "I am fresh and young", "utf8");
     console.log("File created successfully");
   } catch (error) {
