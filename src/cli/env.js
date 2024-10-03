13569c078b5902482de213fb8d02cacd7e5c4335
Max <KontinentKM@gmail.com>
2024-10-03 17:10:30 +0300

    feat: cli/env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+  const rssVariables = Object.entries(process.env)
+      .filter(([key]) => key.startsWith("RSS_"))
+      .map(([key, value]) => `${key}=${value}`);
+
+  console.log(rssVariables.join("; "));
 };
 
 parseEnv();
