cd8c990f81e1d5c8c99b204acc6d9cec3390635a
Max <KontinentKM@gmail.com>
2024-10-04 16:37:55 +0300

    feat: zip/compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream/promises';
+import path from 'path';
+
 const compress = async () => {
-    // Write your code here 
+  try {
+    const filePath = path.join(process.cwd(), 'src/zip/files', 'fileToCompress.txt');
+    const compressedFilePath = path.join(process.cwd(), 'src/zip/files', 'archive.gz');
+
+    const readStream = createReadStream(filePath);
+    const gzipStream = createGzip();
+    const writeStream = createWriteStream(compressedFilePath);
+
+    await pipeline(readStream, gzipStream, writeStream);
+
+    console.log('File compressed successfully!');
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
 };
 
 await compress();
