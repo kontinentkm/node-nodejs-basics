76f200adcd96059e33c062123a300d770819b53f
Max <KontinentKM@gmail.com>
2024-10-04 14:33:32 +0300

    feat: streams/transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    const reverseStream = new Transform({
+        transform(chunk, encoding, callback) {
+            const chunkWithoutNewline = chunk.toString().trim();
+            const reversedChunk = chunkWithoutNewline.split('').reverse().join('') + '\n';
+            callback(null, reversedChunk);
+        }
+    });
+
+    process.stdin.pipe(reverseStream).pipe(process.stdout);
 };
 
 await transform();
