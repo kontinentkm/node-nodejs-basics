dddb1bcee238c7f40532e072e3774376980cf28c
Max <KontinentKM@gmail.com>
2024-10-04 14:23:21 +0300

    feat: streams/write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,20 @@
+import { createWriteStream } from 'fs';
+import { join } from 'path';
+
 const write = async () => {
-    // Write your code here 
+    const filePath = join(process.cwd(), 'src/streams/files', 'fileToWrite.txt');
+
+    const writeStream = createWriteStream(filePath);
+
+    process.stdin.pipe(writeStream);
+
+    writeStream.on('error', (error) => {
+        console.error('Ошибка при записи в файл:', error.message);
+    });
+
+    writeStream.on('finish', () => {
+        console.log('Данные успешно записаны в файл');
+    });
 };
 
 await write();
